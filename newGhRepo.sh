195b9fad7f6e41a807660c905d596870ce29d619
German Cocca <coccagerman@gmail.com>
2022-04-03 12:09:36 -0300

    .

--- newGhRepo.sh
+++ newGhRepo.sh
@@ -5,7 +5,6 @@ while [ -z "$repoName" ]
 do
     echo 'Provide a repository name'
     read -r -p $'Repository name:' repoName
-    # repoName=$repoName1
 done
 
 echo "# $repoName" >> README.md
@@ -16,9 +15,7 @@ git commit -m "Creating remote repo"
 
 curl -u coccagerman https://api.github.com/user/repos -d '{"name": "'"$repoName"'", "private":false}'
 
-GIT_URL=$(curl \
-  -H "Accept: application/vnd.github.v3+json" \
-  https://api.github.com/repos/coccagerman/"$repoName" | jq -r '.clone_url')
+GIT_URL=$(curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/coccagerman/"$repoName" | jq -r '.clone_url')
 
 git branch -M main
 git remote add origin $GIT_URL
